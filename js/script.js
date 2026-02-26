a2b77e0e1ffd95fc91c5d9e07c254c00d5692701
isha0207 <isha95419@gmail.com>
2026-02-26 23:27:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,19 +6,10 @@ const links = document.querySelectorAll(".nav-links a");
 
 hamburger.addEventListener("click", () => {
   body.classList.toggle("nav-open");
-
-  // Scroll Lock
-  if (body.classList.contains("nav-open")) {
-    body.style.overflow = "hidden";
-  } else {
-    body.style.overflow = "auto";
-  }
 });
 
-// Auto Close
 links.forEach(link => {
   link.addEventListener("click", () => {
     body.classList.remove("nav-open");
-    body.style.overflow = "auto";
   });
 });
