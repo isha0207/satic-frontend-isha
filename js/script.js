f1f85cb97704460e561b30da1b50e59545388e22
isha0207 <isha95419@gmail.com>
2026-02-27 00:54:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
 console.log("Navbar Working");
-
-const hamburger = document.querySelector(".hamburger");
-const navbar = document.querySelector(".navbar");
-const links = document.querySelectorAll(".nav-links a");
-
-hamburger.addEventListener("click", () => {
-  navbar.classList.toggle("nav-open");
-});
-
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    navbar.classList.remove("nav-open");
-  });
-});
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  x.classList.toggle("responsive");
+}
