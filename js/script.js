a563c70c6f37954407ff125d2dba51441327a3ed
isha0207 <isha95419@gmail.com>
2026-02-26 21:20:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
 console.log("Day 1 Task Loaded");
-<script>
-  const hamburger = document.querySelector(".hamburger");
-  const navLinks = document.querySelectorAll(".nav-links a");
-  const body = document.body;
 
-  hamburger.addEventListener("click", () => {
-    body.classList.toggle("nav-open");
+const hamburger = document.querySelector(".hamburger");
+const body = document.body;
+const links = document.querySelectorAll(".nav-links a");
 
-    // Scroll Lock (Handled via JS as required)
-    if (body.classList.contains("nav-open")) {
-      body.style.overflow = "hidden";
-    } else {
-      body.style.overflow = "auto";
-    }
+hamburger.addEventListener("click", () => {
+  body.classList.toggle("nav-open");
+
+  // Scroll Lock
+  if (body.classList.contains("nav-open")) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "auto";
+  }
+});
+
+// Auto Close on link click
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    body.classList.remove("nav-open");
+    body.style.overflow = "auto";
   });
-
-  // Auto Close on Link Click
-  navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-      body.classList.remove("nav-open");
-      body.style.overflow = "auto";
-    });
-  });
-</script>
+});
