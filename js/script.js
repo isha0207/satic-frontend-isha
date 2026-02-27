3c828ae58b82cc9c09e7b00b25c358e52731df8c
isha0207 <isha95419@gmail.com>
2026-02-27 16:45:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-console.log("Day 1 Task Loaded");
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-    // Toggle navigation visibility
-    navLinks.classList.toggle('active');
-    
-    // Optional: Animate hamburger bars to an 'X'
-    hamburger.classList.toggle('toggle');
-});
+function toggleMenu() {
+  const nav = document.getElementById("navMenu");
+  nav.classList.toggle("active");
+}
