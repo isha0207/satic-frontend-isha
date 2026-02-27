59fe5a979c4a768895bd5dab78cbc200b488d8ad
isha0207 <isha95419@gmail.com>
2026-02-27 16:25:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
 console.log("Day 1 Task Loaded");
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
 
-const hamburger = document.querySelector(".hamburger");
-const header = document.querySelector(".header");
-const navLinks = document.querySelectorAll(".nav-links a");
-
-hamburger.addEventListener("click", () => {
-  header.classList.toggle("nav-open");
-  document.body.classList.toggle("nav-open");
-});
-
-// Auto close when link clicked
-navLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    header.classList.remove("nav-open");
-    document.body.classList.remove("nav-open");
-  });
+hamburger.addEventListener('click', () => {
+    // Toggle navigation visibility
+    navLinks.classList.toggle('active');
+    
+    // Optional: Animate hamburger bars to an 'X'
+    hamburger.classList.toggle('toggle');
 });
