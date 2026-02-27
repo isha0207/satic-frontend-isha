bbe20b0697a3edf065f7d153a6f1f477ffbfb614
isha0207 <isha95419@gmail.com>
2026-02-27 17:57:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const navMenu = document.getElementById("navMenu");
 
   if (menuBtn && navMenu) {
-
     menuBtn.addEventListener("click", function () {
       navMenu.classList.toggle("show");
     });
-
-  } else {
-    console.log("Menu button or nav not found");
   }
 
 });
