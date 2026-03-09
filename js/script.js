9c5c18109a741d3051e387d4c6a189ded1783a25
isha0207 <isha95419@gmail.com>
2026-03-09 22:04:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -214,15 +214,3 @@ backToTopBtn.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
-document.addEventListener("DOMContentLoaded", function () {
-
-  const menuToggle = document.getElementById("menuToggle");
-  const navMenu = document.getElementById("navMenu");
-
-  if(menuToggle && navMenu){
-    menuToggle.addEventListener("click", function () {
-      navMenu.classList.toggle("show");
-    });
-  }
-
-});
